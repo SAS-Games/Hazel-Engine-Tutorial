5940bd249f05732a985e22fd23aaaa045f99191a
Abhi86Shek <sonkar.abhishek86@gmail.com>
2024-04-07 10:51:30 +0530

    spd Log as submodule

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,6 +5,9 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int args, char** argv)
 {
+	Hazel::Log::Init();
+	HZ_CORE_ERROR("Initialize Core Logger!");
+	HZ_INFO("Initialize Client Logger!!");
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
